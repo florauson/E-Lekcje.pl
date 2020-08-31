f2d483046fa74d5d3923c580ef5c001d0a81a95a
Przemysław <przemyslaw.florczyk@gmail.com>
2020-08-31 14:43:03 +0200

    stare zmiany - nie wiem co tam było xd

--- js/pay_style.js
+++ js/pay_style.js
@@ -1,19 +1,48 @@
 var r_przed = document.querySelector("select[name='przedmiot']");
-var ilosc = document.querySelector('#ilosc');
-var cena = document.querySelector('#cena');
-var cena_poj = document.querySelector('#cena_poj');
-r_przed.addEventListener('change', oblicz);
-ilosc.addEventListener('change', oblicz);
-ilosc.addEventListener('keyup', oblicz);
-var cena_podstawowy = 45;
-var cena_specjalny = 57;
+var ilosc = document.querySelector("#ilosc");
+var cena = document.querySelector("#cena");
+var znizka = document.querySelector("#znizka");
+var cena_poj = document.querySelector("#cena_poj");
+var do_zap = document.querySelector("#do_zap");
+r_przed.addEventListener("change", oblicz);
+ilosc.addEventListener("change", oblicz);
+ilosc.addEventListener("keyup", oblicz);
+var cena_podstawowy = 59;
+var cena_specjalny = 69;
 
 function oblicz(e) {
-  if (r_przed.value == 'pod') {
+  if (r_przed.value == "pod") {
     cena_poj.value = cena_podstawowy;
-    cena.value = cena_podstawowy * ilosc.value;
-  } else if (r_przed.value == 'spec') {
+
+    if (ilosc.value >= 5) {
+      znizka.value = itlosc.value * 5;
+    } else {
+      znizka.value = 0;
+    }
+
+    cena.value = cena_poj.value * ilosc.value;
+    do_zap.value = cena.value - znizka.value;
+  } else if (r_przed.value == "spec") {
     cena_poj.value = cena_specjalny;
-    cena.value = cena_specjalny * ilosc.value;
+    if (ilosc.value >= 5) {
+      znizka.value = ilosc.value * 5;
+    } else {
+      znizka.value = 0;
+    }
+
+    cena.value = cena_poj.value * ilosc.value;
+    do_zap.value = cena.value - znizka.value;
   }
+  // else if (r_przed.value == 'inny') {
+  //   cena_poj.removeAttribute("readonly");
+  //   if (ilosc.value >= 5) {
+  //     znizka.value = (ilosc.value * 5);
+  //   } else {
+  //     znizka.value = 0;
+  //   }
+
+  //   cena.value = cena_poj.value * ilosc.value;
+  //   do_zap.value = cena.value - znizka.value;
+
+  // }
 }
--- js/text_animation.js
+++ js/text_animation.js
@@ -11,7 +11,7 @@ var TxtRotate = function (el, toRotate, period) {
 TxtRotate.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
-  s
+
   if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
